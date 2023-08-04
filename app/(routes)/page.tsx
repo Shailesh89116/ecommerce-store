2539f8228c68cdafdddda43767982a44d663e585
Shailesh Gehlot <shailesh.gehlot.sg@gmail.com>
2023-08-05 02:34:18 +0530

    Update page.tsx

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,7 +8,7 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
+  const billboard = await getBillboard("bffc6c89-d428-4a96-8a76-7c1fc34f4cba");
 
   return (
     <Container>
